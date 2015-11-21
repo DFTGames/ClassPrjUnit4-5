bfec66d4580f8cdbca412bac8a36aa12f4b6169e
Luca Salardi <luca_salardi@yahoo.it>
2015-11-21 12:37:38 +0100

    Implementazione di un  Gestore Percorso

---
+++ ClassPrj/Assets/_Game/Scripts/GeneraPercorso.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum nemiciTipo
+{ 
+    globlin1,
+    globlin2,
+    globlin3
+}
+
+public class GeneraPercorso : MonoBehaviour
+{
+    public Transform[] Percorso;
+    public nemiciTipo nemico = nemiciTipo.globlin1;
+    public int numPoint = 2;
+    public float size = 0.1f;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 1; i <= numPoint; i++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.name = i.ToString();
+            cube.transform.localScale = new Vector3(size, size, size);
+            Renderer mesh = cube.GetComponent<MeshRenderer>();
+            mesh.material.color = Color.red;
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
