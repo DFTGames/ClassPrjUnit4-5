fbf6bb5329590c115ae668b0b68bc494ad84a6a9
Stefano Lippi <Stefano@ingioco.net>
2015-11-21 20:11:35 +0100

    Stato Pattugliamento

---
+++ ClassPrj/Assets/_Game/Scripts/ControllerAI/GeneraPercorso.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TipoPercorso
+{
+    A,
+    B,
+    C
+}
+
+
+public class GeneraPercorso : MonoBehaviour
+{
+
+
+    public Transform[] PercorsoA;
+    public Transform[] PercorsoB;
+    public Transform[] PercorsoC;
+
+    private Transform[] percorso;
+
+
+    public Transform[] Itinerario(TipoPercorso sceltaPercorso)
+    {
+        switch (sceltaPercorso)
+        {
+            case TipoPercorso.A:
+                percorso = PercorsoA;
+                break;
+            case TipoPercorso.B:
+                percorso = PercorsoB;
+                break;
+            case TipoPercorso.C:
+                percorso = PercorsoC;
+                break;
+            default:
+                break;
+        }
+
+        return (percorso);
+    }
+
+
+}
+
