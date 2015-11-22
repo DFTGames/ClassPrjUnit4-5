d2852ace64eac5e3b9dc50b1b9c2c4d1b7353f0f
Pierluigi Coppola <pierluigico@gmail.com>
2015-11-22 01:04:53 +0000

    Aggiunta classe Inseguimento (IStato)

---
+++ ClassPrj/Assets/_Game/Scripts/ControllerAI/Inseguimento.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Inseguimento : IStato
+{
+    public string Nome { get; set; }
+
+    private FSM Cervello;
+    private NavMeshAgent agente;
+    private GameObject PersonaggioDaInseguire;
+
+    public void Inizializza(FSM oggetto)
+    {
+        Cervello = oggetto;
+        agente = Cervello.GetComponent<NavMeshAgent>();
+        PersonaggioDaInseguire = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void PreparoEsecuzione()
+    {
+
+    }
+
+    public void Esecuzione()
+    {
+        agente.destination = PersonaggioDaInseguire.transform.position;
+    }
+
+    public void EsecuzioneTerminata()
+    {
+
+    }
+}
