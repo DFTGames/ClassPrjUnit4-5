ef116205e13e003282db6b9fef5071272c1eb07e
Luca Salardi <luca_salardi@yahoo.it>
2015-11-26 18:24:20 +0100

    Editor  cervello migliorato ..(non ho implementato la visualizzazione del arco per quanto riguarda le armi perche ho scelto di farlo in un secondo momento quando si crea la matrice delle armi)

--- ClassPrj/Assets/_Game/Scripts/Editor/FSMEditor.cs
+++ ClassPrj/Assets/_Game/Scripts/Editor/FSMEditor.cs
@@ -29,45 +29,47 @@ namespace DFTGames.Tools.EditorTools
             EditorGUILayout.Separator();
             EditorGUILayout.BeginVertical(EditorStyles.objectFieldThumb);
             EditorGUILayout.LabelField("IMPOSTAZIONE ARCO E DISTANZA VISIVA");
-            bool tmpBool = EditorGUILayout.Toggle(new GUIContent("Vis. Handle Vista", "Visualizza nella scena gli Handle della Vista"), vista.visualizzaHandleVista);
+            bool tmpBool = EditorGUILayout.Toggle(new GUIContent("Vis. Area Vista", "Visualizza nella scena gli Handle della Vista"), vista.visualizzaHandleVista);
             if (tmpBool != vista.visualizzaHandleVista)
             {
                 vista.visualizzaHandleAttacco = false;  //PER ORA FATTO PROVVISORIO..QUANDO CI SARAN PIU' ARMI SI FA UNA COSA DIVERSA
                 isDirty = true;
-                Undo.RecordObject(vista, "Dimensione Handle Vista");
+                Undo.RecordObject(vista, "Vis. Handle Vista");
                 vista.visualizzaHandleVista = tmpBool;
             }
+            if (vista.visualizzaHandleVista)
+            {
+                tmpDist = EditorGUILayout.Slider(new GUIContent("Distanza Visibilita", "Quanto ci vedo senza occhiali"),
+                    vista.quantoCiVedoSenzaOcchiali, 0.1f, 100f);
+                if (tmpDist != vista.quantoCiVedoSenzaOcchiali)
+                {
+                    isDirty = true;
+                    Undo.RecordObject(vista, "Distanza Visibilita");
+                    vista.quantoCiVedoSenzaOcchiali = tmpDist;
+                }
 
-            tmpDist = EditorGUILayout.Slider(new GUIContent("Distanza Visibilita", "Quanto ci vedo senza occhiali"),
-                vista.quantoCiVedoSenzaOcchiali, 0.1f, 100f);
-            if (tmpDist != vista.quantoCiVedoSenzaOcchiali)
-            {
-                isDirty = true;
-                Undo.RecordObject(vista, "Distanza Visibilita");
-                vista.quantoCiVedoSenzaOcchiali = tmpDist;
-            }
+                tmpArc = EditorGUILayout.Slider(new GUIContent("Dimensione Arco", "Dimensione Angolo arco"), vista.alphaGrad, 5f, 175f);
+                if (tmpArc != vista.alphaGrad)
+                {
+                    isDirty = true;
+                    Undo.RecordObject(vista, "Dimensioni Arco");
+                    vista.alphaGrad = tmpArc;
+                }
 
-            tmpArc = EditorGUILayout.Slider(new GUIContent("Dimensione Arco", "Dimensione Angolo arco"), vista.alphaGrad, 5f, 175f);
-            if (tmpArc != vista.alphaGrad)
-            {
-                isDirty = true;
-                Undo.RecordObject(vista, "Dimensioni Arco");
-                vista.alphaGrad = tmpArc;
+                float tmpHanD1 = EditorGUILayout.Slider(new GUIContent("Dimensione Handle Vista", "Imposta la dimensione degli handle Vista in questo pannello"),
+                   vista.dimensioneHandleVista, 0.1f, 15f);
+                if (tmpHanD1 != vista.dimensioneHandleVista)
+                {
+                    isDirty = true;
+                    Undo.RecordObject(vista, "Distanza Handle Vista");
+                    vista.dimensioneHandleVista = tmpHanD1;
+                }                          
             }
-           
-            float tmpHanD1 = EditorGUILayout.Slider(new GUIContent("Dimensione Handle Vista", "Imposta la dimensione degli handle Vista in questo pannello"),
-               vista.dimensioneHandleVista, 0.1f, 15f);
-            if (tmpHanD1 != vista.dimensioneHandleVista)
-            {
-                isDirty = true;
-                Undo.RecordObject(vista, "Distanza Visibilita");
-                vista.dimensioneHandleVista = tmpHanD1;
-            }
-            
             EditorGUILayout.EndVertical();
             EditorGUILayout.Separator();
             EditorGUILayout.BeginVertical(EditorStyles.objectFieldThumb);
             EditorGUILayout.Separator();
+
             EditorGUILayout.LabelField("IMPOSTAZIONE AMPIEZZA E VELOCITA VISIVA");
             tmpAmp = EditorGUILayout.Slider(new GUIContent("Ampiezza Percezione Vista", "Dimensione Ampiezza"), vista.ampiezza, 0f, 5f);
             if (tmpAmp != vista.ampiezza)
@@ -91,41 +93,42 @@ namespace DFTGames.Tools.EditorTools
             EditorGUILayout.Separator();
 
             EditorGUILayout.LabelField("IMPOSTAZIONE DISTANZA ARCO E SPADA");
-            bool tmpBol = EditorGUILayout.Toggle(new GUIContent("Vis. Handle Distanza Attacco", "Visualizza nella scena gli Handle della Distanza Attacco"), vista.visualizzaHandleAttacco);
+            bool tmpBol = EditorGUILayout.Toggle(new GUIContent("Vis. Area Attacco", "Visualizza nella scena gli Handle della Distanza Attacco"), vista.visualizzaHandleAttacco);
             if (tmpBol != vista.visualizzaHandleAttacco)
             {
                 vista.visualizzaHandleVista = false;  //PER ORA FATTO PROVVISORIO..QUANDO CI SARAN PIU' ARMI SI FA UNA COSA DIVERSA
                 isDirty = true;
-                Undo.RecordObject(vista, "Visual Handle Dist Attacco");
+                Undo.RecordObject(vista, "Vis. Handle Area Attacco");
                 vista.visualizzaHandleAttacco = tmpBol;
             }
-
-            tmpDistArco = EditorGUILayout.Slider(new GUIContent("Arco Distanza Attacco", "Distanza Di Attacco del Arco Goblin"), vista.distanzaAttaccoGoblinArco, 0.1f, 30f);
-            if (tmpDistArco != vista.distanzaAttaccoGoblinArco)
+            if (vista.visualizzaHandleAttacco)
             {
-                isDirty = true;
-                Undo.RecordObject(vista, "Arc Dist Attacco");
-                vista.distanzaAttaccoGoblinArco = tmpDistArco;
-            }
+                tmpDistArco = EditorGUILayout.Slider(new GUIContent("Arco Distanza Attacco", "Distanza Di Attacco del Arco Goblin"), vista.distanzaAttaccoGoblinArco, 0.1f, 30f);
+                if (tmpDistArco != vista.distanzaAttaccoGoblinArco)
+                {
+                    isDirty = true;
+                    Undo.RecordObject(vista, "Arc Dist Attacco");
+                    vista.distanzaAttaccoGoblinArco = tmpDistArco;
+                }
 
-            tmpDistSpada = EditorGUILayout.Slider(new GUIContent("Spada Distanza Attacco", "Distanza Di Attacco del Spada Goblin"), vista.distanzaAttaccoGoblinSpada, 0.1f, 30f);
-            if (tmpDistSpada != vista.distanzaAttaccoGoblinSpada)
-            {
-                isDirty = true;
-                Undo.RecordObject(vista, "Spada Dist Attacco");
-                vista.distanzaAttaccoGoblinSpada = tmpDistSpada;
+                tmpDistSpada = EditorGUILayout.Slider(new GUIContent("Spada Distanza Attacco", "Distanza Di Attacco del Spada Goblin"), vista.distanzaAttaccoGoblinSpada, 0.1f, 30f);
+                if (tmpDistSpada != vista.distanzaAttaccoGoblinSpada)
+                {
+                    isDirty = true;
+                    Undo.RecordObject(vista, "Spada Dist Attacco");
+                    vista.distanzaAttaccoGoblinSpada = tmpDistSpada;
+                }
+                float tmpHanD2 = EditorGUILayout.Slider(new GUIContent("Dimensione Handle DistArmi", "Imposta la dimensione degli handle Distanza armi in questo pannello"),
+                  vista.dimensioneHandleDistArmi, 1f, 15f);
+                if (tmpHanD2 != vista.dimensioneHandleDistArmi)
+                {
+                    isDirty = true;
+                    Undo.RecordObject(vista, "Dimensione Handle Distanza Armi");
+                    vista.dimensioneHandleDistArmi = tmpHanD2;
+                }
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.Separator();
             }
-            float tmpHanD2 = EditorGUILayout.Slider(new GUIContent("Dimensione Handle DistArmi", "Imposta la dimensione degli handle Distanza armi in questo pannello"),
-              vista.dimensioneHandleDistArmi, 1f, 15f);
-            if (tmpHanD2 != vista.dimensioneHandleDistArmi)
-            {
-                isDirty = true;
-                Undo.RecordObject(vista, "Dimensione Handle Distanza Armi");
-                vista.dimensioneHandleDistArmi = tmpHanD2;
-            }
-            EditorGUILayout.EndVertical();
-            EditorGUILayout.Separator();
-
             if (GUI.changed || isDirty)
             {
                 EditorUtility.SetDirty(vista);
@@ -185,8 +188,9 @@ namespace DFTGames.Tools.EditorTools
                     Undo.RecordObject(fsm_, " Arc Dist Attacco");
                 }
 
-                Handles.color = Color.red;
-                Handles.DrawLine(fsm_.transform.position, new Vector3(fsm_.transform.position.x, fsm_.transform.position.y, fsm_.transform.position.z + fsm_.distanzaAttaccoGoblinArco));
+                Handles.color = new Color(0.8f,0.3f,0.5f,0.2f); 
+                Handles.DrawSolidDisc(fsm_.transform.position, Vector3.up, fsm_.distanzaAttaccoGoblinArco);
+
                 fsm_.distanzaAttaccoGoblinSpada = Mathf.Clamp(Handles.ScaleValueHandle(fsm_.distanzaAttaccoGoblinSpada,
                    fsm_.transform.position + (fsm_.transform.forward * fsm_.distanzaAttaccoGoblinSpada),
                      fsm_.transform.rotation, fsm_.dimensioneHandleDistArmi, Handles.ConeCap,
@@ -199,8 +203,9 @@ namespace DFTGames.Tools.EditorTools
                     Undo.RecordObject(fsm_, " Spada Dist Attacco");
                 }
 
-                Handles.color = Color.blue;
-                Handles.DrawLine(fsm_.transform.position, new Vector3(fsm_.transform.position.x, fsm_.transform.position.y, fsm_.transform.position.z + fsm_.distanzaAttaccoGoblinSpada));
+                Handles.color = new Color(1,0.92f,0.016f,0.2f);
+                Handles.DrawSolidDisc(fsm_.transform.position, Vector3.up, fsm_.distanzaAttaccoGoblinSpada);
+               
             }
 
             if (GUI.changed || isDirty)
