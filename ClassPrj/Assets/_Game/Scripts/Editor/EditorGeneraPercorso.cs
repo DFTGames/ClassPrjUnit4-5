3bbf70b5c34de1f801cd7d7b77d8ec800b3dad46
Luca Salardi <luca_salardi@yahoo.it>
2015-12-09 22:52:35 +0100

    CORREZIONE ERRORI DEL MIO GESTORE PERCORSO

--- ClassPrj/Assets/_Game/Scripts/Editor/EditorGeneraPercorso.cs
+++ ClassPrj/Assets/_Game/Scripts/Editor/EditorGeneraPercorso.cs
@@ -17,25 +17,25 @@ public class EditorGeneraPercorso : Editor
 
     private void OnSceneGUI()
     {
+        // Modifica Per Cambiare colori della texture (OPERAZIONE MOLTO ESOSA COME RISORSE..USARE CON CAUTELA)
+        Texture2D text = (Texture2D)ResourceHelper.Icon2;
+        Color tmpColor = me.colore;
+        for (int y = 0; y < text.height; y++)
+        {
+            for (int x = 0; x < text.width; x++)
+            {
+                text.SetPixel(x, y, tmpColor);
+
+            }
+        }
+        text.Apply();
+        //FINE
         for (int i = 1; i < me.transform.childCount; i++)
         {
             me.transform.GetChild(i - 1).name = " Nodo" + (i ); //queste 2 linee fanno in modo che se cambio ordine dei nodi la prox volta che seleziono
-            me.transform.GetChild(i).name = " Nodo" + (i +1);  // il percorso lui mi riordina i nomi correttamente
-            // Modifica Per Cambiare colori della texture
-            Texture2D text= (Texture2D)ResourceHelper.Icon2;
-            Color tmpColor = me.colore;
-            for (int y = 0; y < text.height; y++)
-            {
-                for (int x = 0; x < text.width; x++)
-                {
-                    text.SetPixel(x, y, tmpColor);
-
-                }
-            }
-            text.Apply();
-            Handles.DrawAAPolyLine(text, HandleUtility.GetHandleSize(me.transform.GetChild(i - 1).position), me.transform.GetChild(i - 1).position, me.transform.GetChild(i).position);
-            //fine Modifica
-            //Originale==>   Handles.DrawAAPolyLine((Texture2D)ResourceHelper.Icon2, HandleUtility.GetHandleSize(me.transform.GetChild(i - 1).position), me.transform.GetChild(i-1).position,me.transform.GetChild(i).position);
+            me.transform.GetChild(i).name = " Nodo" + (i +1);  // il percorso lui mi riordina i nomi correttamente  
+            Handles.DrawAAPolyLine(text, HandleUtility.GetHandleSize(me.transform.GetChild(i - 1).position), me.transform.GetChild(i - 1).position, me.transform.GetChild(i).position);          
+           //Originale==>   Handles.DrawAAPolyLine((Texture2D)ResourceHelper.Icon2, HandleUtility.GetHandleSize(me.transform.GetChild(i - 1).position), me.transform.GetChild(i-1).position,me.transform.GetChild(i).position);
         }
         Event e = Event.current;
         Vector2 mousePos = e.mousePosition;
