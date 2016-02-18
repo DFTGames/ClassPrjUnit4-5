62b5ee70f5a85ce35aa3cb4fc20d0004742824c0
Luca Salardi <luca_salardi@yahoo.it>
2016-02-18 18:57:52 +0100

    Effettuato modifiche pattugliamento..

--- ClassPrj/Assets/_Game/Scripts/ControllerAI/Pattugliamento.cs
+++ ClassPrj/Assets/_Game/Scripts/ControllerAI/Pattugliamento.cs
@@ -10,11 +10,9 @@ public class Pattugliamento : IStato
 
     private int indiceDestinazioni = 0;
     private FSM MioCervello;
-    private NavMeshAgent Agente;
-    private Transform ProssimaDirezione;
 
     private GestorePercorso Percorso=null;
-    private Animator Animatore;
+
 
     private Serializzabile<AmicizieSerializzabili> datiDiplomazia;
  
@@ -24,16 +22,25 @@ public class Pattugliamento : IStato
         
         if (Percorso != null && Percorso.transform.childCount>0)
         {
-            Agente.stoppingDistance = 0.8f;
-            if (Agente.remainingDistance <= Agente.stoppingDistance)
+            MioCervello.Agente.stoppingDistance = 0.8f;
+            if (MioCervello.Agente.remainingDistance <= MioCervello.Agente.stoppingDistance)
             {    
                 indiceDestinazioni = indiceDestinazioni < Percorso.transform.childCount ? ++indiceDestinazioni : 0;
                 Transform tmpPercorso = Percorso[indiceDestinazioni]; //Se nessun Nodo e' stato impostato come percorso
-                if (tmpPercorso == null) return;
-                ProssimaDirezione = tmpPercorso;
-                Agente.SetDestination(ProssimaDirezione.position);                
+                if (tmpPercorso == null)
+                {
+                    Debug.LogError("!! SEI Proprio UN PIRLA!! e pure cazzone..Non ci sono nodi per questo percorso");
+                    return;
+
+                }
+                MioCervello.MiaTransform = tmpPercorso;
+                MioCervello.Agente.SetDestination(MioCervello.MiaTransform.position);                
             }
-            Animatore.SetFloat("Velocita", 0.5f);
+            MioCervello.Animatore.SetFloat("Velocita", 0.5f);
+        }
+        else
+        {
+            Debug.LogError("Testa di minchia! ti sei dimenticato Percorso o Il percorso non ha figli");
         }
     }
 
@@ -47,29 +54,27 @@ public class Pattugliamento : IStato
 
         MioCervello = oggetto;
 
-        if (GameManager.dizionarioPercorsi == null) return;
 
         if (GameManager.dizionarioPercorsi.ContainsKey(MioCervello.gameObject.tag))
             MioCervello.IndexPercorso = GameManager.dizionarioPercorsi[MioCervello.gameObject.tag];
 
         //-Vedere se meglio con impostazione nostra di indiceDestionazioni oppure fare in modo che a ogni chiamata al Gestore PErcorso si incrementa da solo
-        if (MioCervello == null) return;
-        if (MioCervello.IndexPercorso < 0) return;
-        Agente = MioCervello.gameObject.GetComponent<NavMeshAgent>();
-        Animatore = MioCervello.gameObject.GetComponent<Animator>();
 
-        GameObject tm = GameObject.Find("PadrePercorso");
-        if (tm == null) return;
-        PadreGestore tmpObj = tm.GetComponent<PadreGestore>();
-        if (tmpObj == null) return;  //Paranoia luc-Code
-
-        Percorso = tmpObj[MioCervello.IndexPercorso];
-        indiceDestinazioni = -1;
+        if (MioCervello.IndexPercorso < 0) //
+        {
+            Debug.LogError("Non e' stato assegnato nessun percorso..quindi se ne sta fermo in attesa di Input");
+            return;
+        }
+        
+        Percorso = GameManager.padreGestore[MioCervello.IndexPercorso];
+        indiceDestinazioni = -1;  //per convenzione ho assegnato il valore -1 per default (percorso no assegnato)
     }
 
     public void PreparoEsecuzione()
     {
-
+       
+       // Da completare!!
+        
     }
 }
 
